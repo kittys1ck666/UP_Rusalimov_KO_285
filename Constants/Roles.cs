09b8d27543bdd95ebf18040404521112759c9fa7
yukigawa <kittys1ck666@gmail.com>
2026-03-09 00:59:52 +0500

    Добавлены роли для пользователей

---
+++ Constants/Roles.cs
@@ -0,0 +1,12 @@
+namespace UP_CONDI_V5.Constants;
+
+public static class Roles
+{
+    public const string Manager = "Менеджер";
+    public const string Master = "Мастер";
+    public const string Operator = "Оператор";
+    public const string Client = "Клиент";
+
+    public const string ManagerOrOperator = Manager + "," + Operator;
+    public const string ManagerOperatorOrMaster = Manager + "," + Operator + "," + Master;
+}
